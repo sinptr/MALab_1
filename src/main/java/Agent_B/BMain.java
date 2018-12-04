cae64399cd194f43944d73d4dc71937b734352dd
sp <zoneofatombomb@gmail.com>
2018-12-05 01:23:53 +0300

    init

---
+++ src/main/java/Agent_B/BMain.java
@@ -0,0 +1,32 @@
+package Agent_B;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+
+public class BMain extends Agent {
+
+    protected void setup() {
+        System.out.println("Привет! агент "+getAID().getName()+" готов.");
+        addBehaviour(new CyclicBehaviour(this) {
+
+            public void action() {
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    System.out.println(" – " +
+                            myAgent.getLocalName() +
+                            " received: "
+                            + msg.getContent() );
+//Вывод на экран локального имени агента и полученного сообщения
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative( ACLMessage.INFORM );
+                    reply.setContent( "Pong");
+//Содержимое сообщения
+                    send(reply); //отправляем сообщения
+                }
+                block();
+            }
+        });
+    }}
+
